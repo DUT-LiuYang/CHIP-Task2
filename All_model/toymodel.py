cf3e262055b3ac9c50772fccc1442b86062c31df
guoyuankai <guoyuankai@outlook.com>
2018-10-25 20:37:18 +0800

    Revert "Update All_model/toymodel.py"
    This reverts commit f855eb25626cbcb145b7b999f5716eb16a6678e0.

--- All_model/toymodel.py
+++ All_model/toymodel.py
@@ -1,91 +1,38 @@
 from keras.layers import *
-from keras.layers import Dense, Bidirectional, GRU, Dropout, Lambda, BatchNormalization
 from Models.BaseModel import BaseModel
-from keras import Model
-from run import *
+
 
 class Toymodel(BaseModel):
-    def __init__(self):
-
-        args = parse_args()
-        args.optimizer = 'adam'
-        args.loss = 'binary_crossentropy'
-        args.need_char_level = False
-        args.lr = 0.01
-
-        args.save_dir = "../saved_models/"
-        args.word_emb_dir = "../instances/word_embed.txt"
-        args.char_emb_dir = "../instances/char_embed.txt"
-        args.r_dir = "../resource/"
-        args.need_char_level=True
-        super(Toymodel, self).__init__(args)
     def build_model(self):
 
+        word_input_1 = self.Q1 # (?, len)
+        word_input_2 = self.Q2  # (?, len)
+        sentence_embedding_layer = Embedding(self.embedding_matrix.shape[0],
+                                             self.embedding_matrix.shape[1],
+                                             weights=[self.embedding_matrix],
+                                             input_length=self.word_max_len,
+                                             trainable=False,
+                                             name='sentence_embedding_layer')
+        BGRU1 = Bidirectional(GRU(300, return_sequences=True, dropout=0.2, implementation=2))
+        BGRU2 = Bidirectional(GRU(300, return_sequences=False, dropout=0.2, implementation=2))
+        sentence_embedding_1 = sentence_embedding_layer(word_input_1)  # (?, len, 300)
+        sentence_embedding_2 = sentence_embedding_layer(word_input_2)  # (?, len, 300)
 
-        BGRU1 = Bidirectional(GRU(256,
-                                activation='relu',
-                                recurrent_dropout=0.2,
-                                return_sequences=True,
-                                implementation=2,
-                                dropout=0.2))
-        BGRU2 =Bidirectional(GRU(256,
-                                activation='relu',
-                                recurrent_dropout=0.2,
-                                return_sequences=False,
-                                implementation=2,
-                                dropout=0.2))
-
-        Char_BGRU1=Bidirectional(GRU(256,
-                                activation='relu',
-                                recurrent_dropout=0.2,
-                                return_sequences=True,
-                                implementation=2,
-                                dropout=0.2))
-        Char_BGRU2=Bidirectional(GRU(256,
-                                activation='relu',
-                                recurrent_dropout=0.2,
-                                return_sequences=False,
-                                implementation=2,
-                                dropout=0.2))
-
-        encoding1 = BGRU1(self.Q1_emb)
-        encoding2 = BGRU1(self.Q2_emb)
-
-        char_encoding1=Char_BGRU1(self.Q1_char_emb)
-        char_encoding2=Char_BGRU1(self.Q2_char_emb)
+        encoding1 = BGRU1(sentence_embedding_1)
+        encoding2 = BGRU1(sentence_embedding_2)
 
         result1 = BGRU2(encoding1)
         result2 = BGRU2(encoding2)
 
-        char_res1=Char_BGRU2(char_encoding1)
-        char_res2=Char_BGRU2(char_encoding2)
-
-
         mysub = subtract([result1, result2])
         mymut = multiply([result1, result2])
-
-        charsub=subtract([char_res1, char_res2])
-        charmut=multiply([char_res1, char_res2])
-
-
-
         result = concatenate([result1, result2, mymut, mysub])
-        char_res = concatenate([char_res1,char_res2,charsub,charmut])
-
         result = Dropout(0.2)(result)
-        char_res= Dropout(0.2)(char_res)
-
-        char_res=Dense(512, activation='tanh')(char_res)
-        char_res=Dense(256, activation='tanh')(char_res)
         result = Dense(512, activation='tanh')(result)
         result = Dense(256, activation='tanh')(result)
-
-        fin_res=concatenate([result,char_res])
-        predictions = Dense(1, activation='sigmoid')(fin_res)
-
-
-        model = Model(inputs=[self.Q1, self.Q2,self.Q1_char,self.Q2_char], outputs=[predictions])
-        model.summary()
+        predictions = Dense(1, activation='sigmoid')(result)
+        # model = Model(inputs=[word_input_1, word_input_2], outputs=predictions)
+        # model.summary()
         # model.compile(loss=['binary_crossentropy'],optimizer='Adam', metrics=['accuracy'])
 
         return predictions
