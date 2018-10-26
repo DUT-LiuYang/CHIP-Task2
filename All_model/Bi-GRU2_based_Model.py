b70a606ec2dc8cf12fa7168d4f901600f67a8894
DUT-LiuYang <1396924332@qq.com>
2018-10-26 15:31:21 +0800

    模型稳定版（调试中）

--- All_model/Bi-GRU2_based_Model.py
+++ All_model/Bi-GRU2_based_Model.py
@@ -2,7 +2,9 @@ import keras
 from keras.layers import Dense, Bidirectional, GRU, Dropout, Lambda, BatchNormalization
 from run import parse_args
 import argparse
+from utils import PRF, print_metrics
 from Models.BaseModel import BaseModel
+from keras.optimizers import Adam
 
 
 def difference(x):
@@ -25,7 +27,8 @@ class LiuModel1(BaseModel):
         args.optimizer = 'adam'
         args.loss = 'binary_crossentropy'
         args.need_char_level = False
-        args.lr = 0.01
+        args.need_word_level = True
+        args.lr = 0.001
 
         args.save_dir = "../saved_models/"
         args.word_emb_dir = "../instances/word_embed.txt"
@@ -36,16 +39,16 @@ class LiuModel1(BaseModel):
 
     def build_model(self):
         encoding_layer1 = Bidirectional(GRU(256,
-                                        activation='relu',
-                                        recurrent_dropout=0.2,
-                                        return_sequences=True,
-                                        dropout=0.2))
+                                            # activation='relu',
+                                            # recurrent_dropout=0.2,
+                                            return_sequences=True,
+                                            dropout=0.2))
         encoded_sentence_1 = encoding_layer1(self.Q1_emb)  # (?, len, 512)
         encoded_sentence_2 = encoding_layer1(self.Q2_emb)  # (?, len, 512)
 
         encoding_layer2 = Bidirectional(GRU(256,
-                                            activation='relu',
-                                            recurrent_dropout=0.2,
+                                            # activation='relu',    # tanh is better than relu
+                                            # recurrent_dropout=0.2,  # rd ?
                                             return_sequences=False,
                                             dropout=0.2))
 
@@ -58,11 +61,26 @@ class LiuModel1(BaseModel):
 
         x = keras.layers.concatenate([encoded_sentence_1, encoded_sentence_2, x1, x2])
         x = Dense(512, activation='elu')(x)
-        x = BatchNormalization()(x)
+        x = BatchNormalization()(x)     # bn ?
         x = Dropout(rate=0.5)(x)
         predictions = Dense(1, activation='sigmoid')(x)
 
         return predictions
+
+    def one_train(self, epochs, batch_size,
+                  train_data, train_label,
+                  dev_data, dev_label):
+        self.compile_model()
+        for e in range(epochs):
+            history = self.model.fit(train_data, train_label, batch_size=batch_size, verbose=2,
+                                     validation_data=(dev_data, dev_label))
+            dev_out = self.model.predict(dev_data, batch_size=2 * batch_size, verbose=0)
+            metrics = PRF(dev_label, (dev_out > 0.5).astype('int32').reshape([-1]))
+            metrics['epoch'] = e + 1
+            metrics['val_loss'] = history.history['val_loss']
+            print_metrics(metrics,
+                          metrics_type=self.model.__class__.__name__ + self.args.selfname,
+                          save_dir='../logs')
 
 
 if __name__ == '__main__':
